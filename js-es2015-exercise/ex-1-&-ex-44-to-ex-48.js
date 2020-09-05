9d8e8a93a19fac78f9a442d110762a547d471c3a
Varier <Aravind.Varier@team.telstra.com>
2020-09-05 15:11:07 +0530

    Restructure exercises and add questions pdf

--- js-es2015-exercise/ex-1-&-ex-44-to-ex-48.js
+++ js-es2015-exercise/ex-1-&-ex-44-to-ex-48.js
@@ -1,8 +1,4 @@
 // EX-01
-const sum = (...args) => args.reduce((sum,val) => sum += val);
-console.log(sum(1, 2, 3, 4));
-
-// EX-44
 const iPhone11 = {
     name: 'iPhone 11',
     manufacturer: 'Apple',
@@ -40,21 +36,25 @@ const {
 } = iPhone11;
 console.log(name, brand, ram, rearCamera, secondColor);
 
-// EX-45
+// EX-02
 const printPhoneDetails = function()  {
     const { calculateDiscoutedPrice } = this;
     console.log(`Apple iPhone is available at 10% discounted rate of $${calculateDiscoutedPrice.call(this, 10)}`);
 };
 printPhoneDetails.call(iPhone11);
 
-// EX-46
+// EX-03
+const sum = (...args) => args.reduce((sum,val) => sum += val);
+console.log(sum(1, 2, 3, 4));
+
+// EX-04
 const max = (...args) => args.reduce((max, ele) => max = max < ele ? ele : max);
 
-// EX-47
+// EX-05
 const numbers = [ 25, 65, 35, 75, 45 ]
 console.log(max(...numbers));
 
-// EX-48
+// EX-06
 const shallowCopy = { ...iPhone11 };
 const deepCopy = {
     ...iPhone11,
