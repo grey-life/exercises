78129616839d8c5d72ebfd7fadb389a6ee26813a
Varier <Aravind.Varier@team.telstra.com>
2020-09-08 10:24:03 +0530

    Add toggle functionality and update styles

--- training-handson-1/frontend-app/src/articles.ts
+++ training-handson-1/frontend-app/src/articles.ts
@@ -1,8 +1,8 @@
-fetch("http://localhost:3000/articles")
+fetch("http://localhost:3000/articles?include=comments")
     .then(response => response.json())
     .then(articles => {
         const row = <HTMLElement> document.querySelector('.row');
-        articles.forEach((article : any) => {
+        articles.forEach((article : any, id: Number) => {
             row.innerHTML += `
                 <div class="col-lg-4 col-md-6 col-sm-12 my-card">
                     <div class="card">
@@ -11,6 +11,19 @@ fetch("http://localhost:3000/articles")
                             <h5 class="card-title">${article.title}</h5>
                             <p class="card-text">${article.abstract}</p>
                             <p class="card-text"><small class="text-muted">by ${article.author}</small></p>
+                            <button class="btn btn-primary" type="button" data-toggle="collapse" data-target="#Comments${id}" aria-expanded="false" aria-controls="collapseExample">
+                                    Show Comments
+                            </button>
+                            <div class="collapse my-collapse" id="Comments${id}">
+                            ${
+                                article.comments.length > 0 ? `
+                                    ${article.comments.map((comment:any) => `
+                                        <p class="card-text">${comment.commenter} -> ${comment.comment}</p>
+                                    `).join('\n')}
+                                ` 
+                                : '<p class="card-text">No comments yet</p>'
+                            }
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -18,28 +31,4 @@ fetch("http://localhost:3000/articles")
         });
     })
     .catch(error => console.log('error', error));
-
-// const headers: HeadersInit = {
-//     'Content-Type': 'application/json',
-// }
-
-// const raw = JSON.stringify({ 
-//     "name": "Superb Leaf Rake", 
-//     "code": "GDN-0011", 
-//     "releaseDate": "March 19, 2016",
-//     "description": "Leaf rake with 48-inch wooden handle.",
-//     "price": 19.95, "starRating": 3.2, 
-//     "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png" 
-// });
-
-// const requestOptions : RequestInit  = {
-//     method: 'POST',
-//     headers: headers,
-//     body: raw,
-//     redirect: 'follow'
-// };
-
-// fetch("http://localhost:3000/products", requestOptions)
-//     .then(response => response.json())
-//     .then(result => console.log(result))
-//     .catch(error => console.log('error', error));
+    
